5c73c7055c1b9b6bbd022f72335506e9c2d41a10
Antero Oikkonen <antero.oikkonen@gmail.com>
2019-01-24 19:37:47 +0200

    create database test content

--- express-locallibrary-tutorial/models/book.js
+++ express-locallibrary-tutorial/models/book.js
@@ -6,13 +6,15 @@ var Schema = mongoose.Schema;
 
 
 
-var BookSchema = new Schema({
-  title: {tyep: String, required: true },
-  author : { type: Schema.Types.ObjectId, ref: 'Author', required: true },
-  summary: { type: String, required: true },
-  isbn: { type: String,required: true},
-  genre: [{type: Schema.Types.ObjectId, ref: 'Genre'}]
-});
+var BookSchema = new Schema(
+  {
+    title : { type: String, required: true },
+    author : { type: Schema.Types.ObjectId, ref: 'Author', required: true },
+    summary: { type: String, required: true },
+    isbn: { type: String,required: true},
+    genre: [{type: Schema.Types.ObjectId, ref: 'Genre'}]
+  }
+);
 
 // Virtual for book's url
 BookSchema
